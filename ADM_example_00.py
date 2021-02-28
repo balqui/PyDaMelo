697427939495f8451de847c513eae6cb04374d40
balqui <jose.luis.balcazar@upc.edu>
2021-02-28 12:02:45 +0100

    added examples w/o conf mat

---
+++ ADM_example_00.py
@@ -0,0 +1,22 @@
+from data import Data
+from maxapost import MaxAPost
+
+filename = "datasets/weatherNominal.td"
+## filename = "datasets/titanic.td"
+## filename = "datasets/cmc.td"
+
+d = Data(filename)
+d.report()
+
+pr = MaxAPost(d)
+pr.train()
+pr.show()
+
+for (v,c_true) in d.test_set:
+        c_pred = pr.predict(v)[0]
+        print(v, ":")
+        print("   ", c_pred, "( true class:", c_true, ")")
+
+##    print(pr.predict(("Class:1st","Sex:Female","Age:Child")))
+
+##    print(pr.predict(("Class:Crew","Sex:Female","Age:Child")))
